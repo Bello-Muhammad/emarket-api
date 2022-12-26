27a8512062950ebf85d8ac82b33d44cc3b435eae
Bello Muhammad <67134404+Bello-Muhammad@users.noreply.github.com>
2022-12-26 07:09:29 +0000

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,7 +6,7 @@ mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     // useCreateIndex: true,
     // useFindAndModify: false,
-    // useUnifiedTopology: true
+    useUnifiedTopology: true
 })
 
 
