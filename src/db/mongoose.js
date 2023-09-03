1249f9733d0590dafe46cd8f4797821f9f712ae0
Bello Muhammad <67134404+Bello-Muhammad@users.noreply.github.com>
2023-09-03 01:13:56 +0100

    Update mongoose.js

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 
-mongoose.connect(process.env.MONGODB_UR, {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
 })
